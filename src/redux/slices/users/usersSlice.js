680ed20c5cb630f3f336248c7c34d11ad9dc4e62
onurtaskirancom <taskiranonur9@gmail.com>
2024-02-20 15:14:33 +0300

    Users Initial State

---
+++ src/redux/slices/users/usersSlice.js
@@ -0,0 +1,13 @@
+//initialState
+const initialState = {
+  loading: false,
+  error: null,
+  users: [],
+  user: null,
+  profile: {},
+  userAuth: {
+    loading: false,
+    error: null,
+    userInfo: {},
+  },
+};
