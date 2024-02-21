0f563799817db73da671c34e97ebd22680d5eb36
onurtaskirancom <taskiranonur9@gmail.com>
2024-02-21 16:55:48 +0300

    Login Action

--- src/redux/slices/users/usersSlice.js
+++ src/redux/slices/users/usersSlice.js
@@ -1,3 +1,8 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+import baseURL from '../../../utils/baseURL';
+
 //initialState
 const initialState = {
   loading: false,
@@ -11,3 +16,20 @@ const initialState = {
     userInfo: {},
   },
 };
+
+//login action
+export const loginUserAction = createAsyncThunk(
+  'users/login',
+  async ({ email, password }, { rejectWithValue, getState, dispatch }) => {
+    try {
+      //make the http request
+      const { data } = await axios.post(`${baseURL}/users/login`, {
+        email,
+        password,
+      });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
