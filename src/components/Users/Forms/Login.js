c48155208c280021ec84fa3e0a43ae446d6b6d0c
onurtaskirancom <taskiranonur9@gmail.com>
2024-02-27 15:03:08 +0300

    Display Error And Loading After Login

--- src/components/Users/Forms/Login.js
+++ src/components/Users/Forms/Login.js
@@ -28,11 +28,12 @@ const Login = () => {
   );
 
   //redirect
-  if (userInfo?.userFound?.isAdmin) {
-    window.location.href = '/admin';
-  } else {
-    window.location.href = '/customer-profile';
-  }
+  // if (userInfo?.userFound?.isAdmin) {
+  //   window.location.href = '/admin';
+  // } else {
+  //   window.location.href = '/customer-profile';
+  // }
+
   return (
     <>
       <section className="py-20 bg-gray-100 overflow-x-hidden">
@@ -47,6 +48,8 @@ const Login = () => {
                 <p className="mb-10 font-semibold font-heading">
                   Happy to see you again
                 </p>
+                {/* err */}
+                {error && <h2 className="text-red-600">{error?.message}</h2>}
                 <form
                   className="flex flex-wrap -mx-4"
                   onSubmit={onSubmitHandler}
@@ -81,9 +84,18 @@ const Login = () => {
                   </div>
 
                   <div className="w-full px-4">
-                    <button className="bg-blue-800 hover:bg-blue-900 text-white font-bold font-heading py-5 px-8 rounded-md uppercase">
-                      Login
-                    </button>
+                    {loading ? (
+                      <button
+                        disabled
+                        className="bg-gray-800 text-white font-bold font-heading py-5 px-8 rounded-md uppercase"
+                      >
+                        Loading..
+                      </button>
+                    ) : (
+                      <button className="bg-blue-800 hover:bg-blue-900 text-white font-bold font-heading py-5 px-8 rounded-md uppercase">
+                        Login
+                      </button>
+                    )}
                   </div>
                 </form>
               </div>
