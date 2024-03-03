9e4e5faa16a59aa71798ad48eabfe56ea6150e53
onurtaskirancom <taskiranonur9@gmail.com>
2024-03-03 18:20:41 +0300

    Protecting Routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AdminDashboard from "./components/Admin/AdminDashboard";
-import ManageCoupons from "./components/Admin/Coupons/ManageCoupons";
-import AddCoupon from "./components/Admin/Coupons/AddCoupon";
-import Login from "./components/Users/Forms/Login";
-import AddProduct from "./components/Admin/Products/AddProduct";
-import RegisterForm from "./components/Users/Forms/RegisterForm";
-import HomePage from "./components/HomePage/HomePage";
-import Navbar from "./components/Navbar/Navbar";
-import OrderHistory from "./components/Admin/Orders/ManageOrders";
-import OrderPayment from "./components/Users/Products/OrderPayment";
-import ManageCategories from "./components/Admin/Categories/ManageCategories";
-import UpdateProduct from "./components/Admin/Products/UpdateProduct";
-import ManageStocks from "./components/Admin/Products/ManageStocks";
-import CategoryToAdd from "./components/Admin/Categories/CategoryToAdd";
-import AddCategory from "./components/Admin/Categories/AddCategory";
-import AddBrand from "./components/Admin/Categories/AddBrand";
-import AddColor from "./components/Admin/Categories/AddColor";
-import AllCategories from "./components/HomePage/AllCategories";
-import UpdateCoupon from "./components/Admin/Coupons/UpdateCoupon";
-import Product from "./components/Users/Products/Product";
-import ShoppingCart from "./components/Users/Products/ShoppingCart";
-import ProductsFilters from "./components/Users/Products/ProductsFilters";
-import CustomerProfile from "./components/Users/Profile/CustomerProfile";
-import AddReview from "./components/Users/Reviews/AddReview";
-import UpdateCategory from "./components/Admin/Categories/UpdateCategory";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AdminDashboard from './components/Admin/AdminDashboard';
+import ManageCoupons from './components/Admin/Coupons/ManageCoupons';
+import AddCoupon from './components/Admin/Coupons/AddCoupon';
+import Login from './components/Users/Forms/Login';
+import AddProduct from './components/Admin/Products/AddProduct';
+import RegisterForm from './components/Users/Forms/RegisterForm';
+import HomePage from './components/HomePage/HomePage';
+import Navbar from './components/Navbar/Navbar';
+import OrderHistory from './components/Admin/Orders/ManageOrders';
+import OrderPayment from './components/Users/Products/OrderPayment';
+import ManageCategories from './components/Admin/Categories/ManageCategories';
+import UpdateProduct from './components/Admin/Products/UpdateProduct';
+import ManageStocks from './components/Admin/Products/ManageStocks';
+import CategoryToAdd from './components/Admin/Categories/CategoryToAdd';
+import AddCategory from './components/Admin/Categories/AddCategory';
+import AddBrand from './components/Admin/Categories/AddBrand';
+import AddColor from './components/Admin/Categories/AddColor';
+import AllCategories from './components/HomePage/AllCategories';
+import UpdateCoupon from './components/Admin/Coupons/UpdateCoupon';
+import Product from './components/Users/Products/Product';
+import ShoppingCart from './components/Users/Products/ShoppingCart';
+import ProductsFilters from './components/Users/Products/ProductsFilters';
+import CustomerProfile from './components/Users/Profile/CustomerProfile';
+import AddReview from './components/Users/Reviews/AddReview';
+import UpdateCategory from './components/Admin/Categories/UpdateCategory';
 
-import OrdersList from "./components/Admin/Orders/OdersList";
-import ManageOrders from "./components/Admin/Orders/ManageOrders";
-import Customers from "./components/Admin/Orders/Customers";
-import BrandsColorsList from "./components/Admin/Categories/BrandsColorsList";
+import OrdersList from './components/Admin/Orders/OdersList';
+import ManageOrders from './components/Admin/Orders/ManageOrders';
+import Customers from './components/Admin/Orders/Customers';
+import BrandsColorsList from './components/Admin/Categories/BrandsColorsList';
+import AuthRoute from './components/AuthRoute/AuthRoute';
 
 const App = () => {
   return (
@@ -37,8 +38,15 @@ const App = () => {
       <Navbar />
       {/* hide navbar if admin */}
       <Routes>
-        {/* nested route */}
-        <Route path="admin" element={<AdminDashboard />}>
+        {/* admin route */}
+        <Route
+          path="admin"
+          element={
+            <AuthRoute>
+              <AdminDashboard />
+            </AuthRoute>
+          }
+        >
           {/* products */} <Route path="" element={<OrdersList />} />
           <Route path="add-product" element={<AddProduct />} />
           <Route path="manage-products" element={<ManageStocks />} />
@@ -48,7 +56,7 @@ const App = () => {
           <Route path="manage-coupon" element={<ManageCoupons />} />
           <Route path="manage-coupon/edit/:code" element={<UpdateCoupon />} />
           {/* Category */}
-          <Route path="category-to-add" element={<CategoryToAdd />} />{" "}
+          <Route path="category-to-add" element={<CategoryToAdd />} />{' '}
           <Route path="add-category" element={<AddCategory />} />
           <Route path="manage-category" element={<ManageCategories />} />
           <Route path="edit-category/:id" element={<UpdateCategory />} />
