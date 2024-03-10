d0f6a23ddb7b541e0ecac74b8111932a2da3923a
onurtaskirancom <taskiranonur9@gmail.com>
2024-03-10 17:53:36 +0300

    Create Product Slice

--- src/redux/slices/products/productSlices.js
+++ src/redux/slices/products/productSlices.js
@@ -31,6 +31,36 @@ export const createProductAction = createAsyncThunk(
         price,
       });
       return data;
-    } catch (error) {}
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
   }
 );
+
+//slice
+const productSlice = createSlice({
+  name: 'products',
+  initialState,
+  extraReducers: (builder) => {
+    //create
+    builder.addCase(createProductAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(createProductAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.product = action.payload;
+      state.isAdded = true;
+    });
+    builder.addCase(createProductAction.rejected, (state, action) => {
+      state.loading = false;
+      state.product = null;
+      state.isAdded = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+//generate the reducer
+const productReducer = productSlice.reducer;
+
+export default productReducer;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import productReducer from '../slices/products/productSlices';
 import usersReducer from '../slices/users/usersSlice';
 
 //store
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    products: productReducer,
   },
 });
 
