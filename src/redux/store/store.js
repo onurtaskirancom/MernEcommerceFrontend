2a13c4e137d664c4509013f084e0efcbbb9a9059
onurtaskirancom <taskiranonur9@gmail.com>
2024-04-29 02:49:41 +0300

    Create And Fetch All Coupons Actions

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,6 +3,7 @@ import cartReducer from '../slices/cart/cartSlices';
 import brandsReducer from '../slices/categories/brandsSlice';
 import categoryReducer from '../slices/categories/categoriesSlice';
 import colorsReducer from '../slices/categories/colorsSlice';
+import couponsReducer from '../slices/coupons/couponsSlice';
 import productReducer from '../slices/products/productSlices';
 import usersReducer from '../slices/users/usersSlice';
 
@@ -15,6 +16,7 @@ const store = configureStore({
     brands: brandsReducer,
     colors: colorsReducer,
     carts: cartReducer,
+    coupons: couponsReducer,
   },
 });
 
