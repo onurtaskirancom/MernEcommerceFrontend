b590edc5a9db34e9224cab430497bff464539644
onurtaskirancom <taskiranonur9@gmail.com>
2024-06-12 18:35:26 +0300

    Fetch All Colors And Brands Categories

--- src/App.js
+++ src/App.js
@@ -29,12 +29,13 @@ import UpdateCategory from './components/Admin/Categories/UpdateCategory';
 import OrdersList from './components/Admin/Orders/OdersList';
 import ManageOrders from './components/Admin/Orders/ManageOrders';
 import Customers from './components/Admin/Orders/Customers';
-import BrandsColorsList from './components/Admin/Categories/BrandsColorsList';
+import BrandsList from './components/Admin/Categories/BrandsList';
 import AuthRoute from './components/AuthRoute/AuthRoute';
 import AdminRoutes from './components/AuthRoute/AdminRoutes';
 import ThanksForOrdering from './components/Users/Products/ThanksForOrdering';
 import ProductUpdate from './components/Admin/Products/ProuductUpdate';
 import UpdateOrders from './components/Admin/Orders/UpdateOrders';
+import ColorsList from './components/Admin/Categories/ColorsList';
 
 const App = () => {
   return (
@@ -67,10 +68,10 @@ const App = () => {
           <Route path="edit-category/:id" element={<UpdateCategory />} />
           {/* brand category */}
           <Route path="add-brand" element={<AddBrand />} />
-          <Route path="all-brands" element={<BrandsColorsList />} />
+          <Route path="all-brands" element={<BrandsList />} />
           {/* color category */}
           <Route path="add-color" element={<AddColor />} />
-          <Route path="all-colors" element={<BrandsColorsList />} />
+          <Route path="all-colors" element={<ColorsList />} />
           {/* Orders */}
           <Route path="manage-orders" element={<ManageOrders />} />
           <Route path="orders/:id" element={<UpdateOrders />} />
