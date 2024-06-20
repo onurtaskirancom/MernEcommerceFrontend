90b27d9e8fed820de62e130b7c1c3e08feae80a8
onurtaskirancom <taskiranonur9@gmail.com>
2024-06-20 21:10:21 +0300

    Protecting Private Components

--- src/components/AuthRoute/AdminRoutes.js
+++ src/components/AuthRoute/AdminRoutes.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserProfileAction } from '../../redux/slices/users/usersSlice';
+import AdminOnly from '../NotAuthorised/AdminOnly';
 
 const AdminRoutes = ({ children }) => {
-  //get user from localstorage
-  const user = JSON.parse(localStorage.getItem('userInfo'));
-  const isAdmin = user?.userFound?.isAdmin ? true : false;
-  if (!isAdmin) return <h1>Acces Denied, Admin only</h1>;
+  //dispatch
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUserProfileAction());
+  }, [dispatch]);
+  //get user from store
+  const { userAuth } = useSelector((state) => state?.users);
+  const isAdmin = userAuth?.userInfo?.userFound?.isAdmin ? true : false;
+  if (!isAdmin) return <AdminOnly />;
   return <>{children}</>;
 };
 
