1b5bafeda9d3cec01aa2d3ff75ace719f7bc5abd
onurtaskirancom <taskiranonur9@gmail.com>
2024-06-23 05:20:17 +0300

    Display Coupon Days Left

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -183,9 +183,12 @@ export default function Navbar() {
           {/* Top navigation  desktop*/}
           <div className="bg-gray-900">
             <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-              <p className="flex-1 text-center text-sm font-medium text-white lg:flex-none">
+              <p
+                style={{ textAlign: 'center', width: '100%' }}
+                className="flex-1 text-center text-sm font-medium text-white lg:flex-none"
+              >
                 {currentCoupon
-                  ? `${currentCoupon?.code}- ${currentCoupon?.discount}%`
+                  ? `${currentCoupon?.code}- ${currentCoupon?.discount}% , ${currentCoupon?.daysLeft}`
                   : 'No Flash sale at moment'}
               </p>
 
